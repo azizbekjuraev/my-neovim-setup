9826debcf9ecdddb302531900280c25f5dec6778
azizbekjuraev <ajuraev98@mail.ru>
2026-03-09 17:08:33 +0500

    init

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mcchrish/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("nordbones")
+    end,
+  },
+}
