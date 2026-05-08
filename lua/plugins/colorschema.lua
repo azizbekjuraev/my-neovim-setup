db84c8b7574eeb9e9c53399812bed281dc81ddfe
azizbekjuraev <ajuraev98@mail.ru>
2026-05-08 09:56:30 +0500

    updated & fixed errors

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,11 +1,16 @@
 return {
   {
-    "mcchrish/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
+    "neanias/everforest-nvim",
+    version = false,
     lazy = false,
-    priority = 1000,
+    priority = 1000, -- Make sure it loads before everything else
     config = function()
-      vim.cmd.colorscheme("nordbones")
+      require("everforest").setup({
+        background = "soft", -- Choose between "hard", "medium", or "soft"
+        ui_contrast = "low", -- Choose between "low" or "high"
+      })
+      
+      vim.cmd([[colorscheme everforest]])
     end,
   },
 }
